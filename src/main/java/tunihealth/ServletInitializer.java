06f213c26f03e66d6e0face8c5eba8d23306fc4d
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-05-10 14:25:20 +0100

    TuniHealth App

--- src/main/java/tunihealth/ServletInitializer.java
+++ src/main/java/tunihealth/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(HelloDoctorApplication.class);
+		return application.sources(TuniHealthApplication.class);
 	}
 
 }
---
+++ src/main/java/tunihealth/TuniHealthApplication.java
@@ -0,0 +1,13 @@
+package tunihealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuniHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuniHealthApplication.class, args);
+	}
+
+}
